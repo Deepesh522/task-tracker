5178ecc5d780a3c9b7f14f65f204368e22363cc7
storr <deepeshpanwar1212@gmail.com>
2026-06-25 19:22:27 +0530

    updated code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import argparse
 import json
 import os
+import time
 
 TASK_SHEET="task.json"
 
@@ -29,6 +30,16 @@ def save_data(data):
     with open(TASK_SHEET,"w") as file:
         json.dump(data,file,indent=4)
 
+def update_status(task_id,status):
+    data=load_data()
+    for task in data:
+        if task["id"]==task_id:
+            task["status"]=status
+            task["updatedAt"]=time.ctime()
+            save_data(data)
+            return
+    print("Task not found")
+
 def main():
     parser = argparse.ArgumentParser(description="Store user info into Json")
     
@@ -36,7 +47,8 @@ def main():
     add_parser = subparsers.add_parser("add",help="create new task")
     update_parser = subparsers.add_parser("update", help = "update existing task")
     delete_parser = subparsers.add_parser("delete", help = "delete existing task")
-    mark_parser= subparsers.add_parser("mark", help = "change the state of task")
+    mark_inprogress_parser= subparsers.add_parser("mark-in-progress", help = "change the state of task")
+    mark_done_parser= subparsers.add_parser("mark-done", help = "change the state of task")    
     list_parser= subparsers.add_parser("list", help = "List the task as per demand")
     #add parser
     add_parser.add_argument("description", help="description of the new task")
@@ -45,40 +57,81 @@ def main():
     update_parser.add_argument("description", help="updated description for the task")
     #delete pareser
     delete_parser.add_argument("id",type=int,help="task id which needs delete")
-    #mark parser
-    mark_parser.add_argument
+    # parmark_inprogress_parser
+    mark_inprogress_parser.add_argument("id",type=int,help="task id which needs to move to inprogress")
+    #mark
+    mark_done_parser.add_argument("id",type=int,help="task id which needs to move to done")
+    # list 
+    list_parser.add_argument("status", help="status of the task")
 
     args=parser.parse_args()
 
     if args.command=="add":
+        current_time=time.ctime()
         new_entry={
             "id":get_next_id(),
-            "description":args.description
+            "description":args.description,
+            "status":"todo",
+            "createdAt": time.ctime(),
+            "updatedAt":time.ctime()
         }
         data=load_data()
         data.append(new_entry)
         save_data(data)
-        print("# Output: Task added successfully (ID: ",{new_entry['id']},")")
+        print(f"Task added successfully (ID: {new_entry['id']})")
 
 
     if args.command=="update":
         data=load_data()
+        found = False
         for task in data:
             if task.get('id')==args.id:
+                found=True
                 task['description']=args.description
-        save_data(data)
+                task['updatedAt']=time.ctime()
+                break
+        if found:
+            save_data(data)
+            print("# Output: Task updated successfully (ID:{args.id})")
+        else :
+            print("# Output: Task is not present {args.id})")
 
     if args.command=="delete":
         data=load_data()
         updated_data = [task for task in data if task.get('id')!=args.id]
         if len(data)==len(updated_data):
             print("not found")
-        save_data(updated_data)
+        else:
+            save_data(updated_data)
+            print("# Output: Task deleted successfully (ID:{args.id})")
     
-    if args.command=="mark":
+    if args.command=="mark-in-progress":
+        data=load_data()
+        for task in data:
+            if(task['id']==args.id):
+                task['status']='in-progress'
+                task['updatedAt']=time.ctime()
+        save_data(data)
+    
+    if args.command=="mark-done":
+        data=load_data()
+        for task in data:
+            if(task['id']==args.id):
+                task['status']='done'
+                task['updatedAt']=time.ctime()
+        save_data(data)
 
-
-
+    if args.command=="list" and args.status ==None:
+        data=load_data()
+        for task in data:
+            print(task)
+    
+    if args.command=="list" and args.status != None:
+        data=load_data()
+        to_show_task = [task for task in data if task.get('status')==args.status]
+        for t in to_show_task:
+            print(t)
+    
 
 if __name__=="__main__":
     main()
